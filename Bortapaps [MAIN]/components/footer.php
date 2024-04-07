e2eb1a1b7ae1ffaf6d126b1a4c38e6d80951ec47
Xzud <shijirox44@gmail.com>
2024-04-07 21:04:11 +0800

    added alert on added to cart

--- Bortapaps [MAIN]/components/footer.php
+++ Bortapaps [MAIN]/components/footer.php
@@ -208,5 +208,6 @@
   <!-- 
     - ionicon link
   -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
